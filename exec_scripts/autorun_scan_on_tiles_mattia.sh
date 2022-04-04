09f131969de6ad57df0b8878b7c49d589371127e
mattiasoldani <mattiasoldani93@gmail.com>
2022-04-04 16:14:06 +0200

    working on higher statistics production

--- exec_scripts/autorun_scan_on_tiles_mattia.sh
+++ exec_scripts/autorun_scan_on_tiles_mattia.sh
@@ -1,6 +1,6 @@
 # in order to make this work, copy it (& .mac dependencies) into the build directory
 
-Edistr=mono1000  # this is just for filename, actual beam parameter has to be set in PrimaryGeneratorAction manually
+Edistr=mono10  # this is just for filename, actual beam parameter has to be set in PrimaryGeneratorAction manually
 ndistr=flat1-5  # this is just for filename, actual beam parameter has to be set in PrimaryGeneratorAction manually
 
 for nveto in 1 2 3 4; do
@@ -11,10 +11,10 @@ for nveto in 1 2 3 4; do
     
     for particle in gamma ele; do
         
-        ./22_desy_preshower macros/${particle}_100kevs.mac
-        mv out_data/OutData_t0.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000000.root;
-		mv out_data/OutData_t1.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000001.root;
-		out_data/OutData.root
+        ./22_desy_preshower ${particle}_100kevs.mac
+	mv out_data/OutData_t0.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000000.root;
+	mv out_data/OutData_t1.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000001.root;
+	rm out_data/OutData.root
         
     done
     
@@ -30,10 +30,10 @@ for nDC in 1 2 3 4; do
     
     for particle in gamma ele; do
         
-        ./22_desy_preshower macros/${particle}_100kevs.mac
-        mv out_data/OutData_t0.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000000.root;
-		mv out_data/OutData_t1.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000001.root;
-		out_data/OutData.root
+	./22_desy_preshower ${particle}_100kevs.mac
+	mv out_data/OutData_t0.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000000.root;
+	mv out_data/OutData_t1.root out_data/${particle}_${Edistr}_${ndistr}_veto${nveto}_DC${nDC}_000001.root;
+	rm out_data/OutData.root
         
     done
     
